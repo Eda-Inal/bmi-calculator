eb0b0db8a822a9f4fd354abbd2ac4f53eea0881a
Eda Işıl İnal <edainalisil@gmail.com>
2024-07-08 21:14:05 +0300

    add navbar

---
+++ bmi/src/app/components/navbar/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import styles from "./styles.module.css"
+import Link from "next/link"
+import { FaHeartbeat } from "react-icons/fa";
+
+function Navbar() {
+  return (
+<nav className={`${styles.bg} ${styles.font} flex justify-around items-center   mx-auto`}>
+  
+ 
+    <div >
+    {<FaHeartbeat className='text-2xl' />}
+    </div>
+    
+   <ul className= {`flex  items-center gap-4 ${styles.font}`} >
+    <li>
+    <Link className='hover:text-cyan-100' href="./">Ana Sayfa</Link>
+    </li>
+    <li>
+    <Link className='hover:text-cyan-100' href="./">Ana Sayfa</Link>
+    </li>
+    <li>
+    <Link  className='hover:text-cyan-100'href="./">Ana Sayfa</Link>
+    </li>
+
+   </ul>
+ 
+   
+</nav>
+  )
+}
+
+export default Navbar
--- bmi/src/app/layout.jsx
+++ bmi/src/app/layout.jsx
@@ -1,5 +1,6 @@
 
 import "./globals.css";
+import Navbar from "./components/navbar"
 
 
 
@@ -8,6 +9,7 @@ export default function RootLayout({children})
 {
   return (
     <html lang="en">
+      <Navbar/>
       <body>{children}</body>
     </html>
   );
