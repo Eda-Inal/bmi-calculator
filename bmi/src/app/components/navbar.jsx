9768e52244a9d6f71cb2cad9efa7ee3cc7f43ae1
Eda Işıl İnal <edainalisil@gmail.com>
2024-07-10 10:24:36 +0300

    home css

--- bmi/src/app/components/navbar.jsx
+++ bmi/src/app/components/navbar.jsx
@@ -6,7 +6,7 @@ import Link from "next/link"
 
 function Navbar() {
   return (
-<nav className={`bg-red-400 flex items-center text-2xl h-16 sm:justify-between justify-evenly  `}>
+<nav className={`bg-red-400 flex items-center text-white sm:text-2xl text-xl h-16 sm:justify-between justify-evenly  `}>
   <div className='sm:pl-10'>
     
     
@@ -18,10 +18,10 @@ function Navbar() {
         />
   </div>
   <div  >
-   <Link className='sm:pr-10 pr-3 hover:text-gray-700 pl-3' href="/home">Ana Sayfa</Link>
-   <Link className='sm:pr-10 pr-3 hover:text-gray-700' href="/beslenme">Beslenme</Link>
+   <Link className='sm:pr-10 pr-3 hover:text-green-200 pl-3' href="/home">Ana Sayfa</Link>
+   <Link className='sm:pr-10 pr-3 hover:text-green-200' href="/beslenme">Beslenme</Link>
 
-    <Link className='sm:pr-10 hover:text-gray-700 ' href="/contact">İletişim</Link>
+    <Link className='sm:pr-10  hover:text-green-200 ' href="/contact">İletişim</Link>
   </div>
 </nav>
   )
--- bmi/src/app/home/page.jsx
+++ bmi/src/app/home/page.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import body from '../public/body.png';
+import body from '../public/human.png';
 import Image from 'next/image';
 import styles from "./styles.module.css"
 
@@ -7,8 +7,8 @@ import styles from "./styles.module.css"
 function Home() {
   return (
     <div className='flex sm:flex-row flex-col justify-center text-2xl mt-10 '>
-  <div className='w-2/5'>
-  < Image className='sm:w-3/5 w-2/5'
+  <div className='w-2/5 sm:mx-0 mx-auto  sm:mt-20 md:mt-16'>
+  < Image className='sm:w-3/5 w-3/5'
         src={body}
         alt='body'
         />
@@ -19,16 +19,16 @@ function Home() {
 
   <div className='2/5'>
   <div className='flex flex-col  mt-20 text-xl'>
-  <div className='flex flex-row mb-10'>
-    <div className='w-1/2 pr-2'>Boy:</div>
+  <div className='flex flex-row  mb-10'>
+    <div className='sm:w-1/2 w-1/5  ml-20  sm:ml-0 pr-2'>Boy:</div>
     <div>
     <input type="text" 
-className='bg border-gray-500 border-2  w-40 pl-1 ' />
+className='bg border-gray-500 border-2 w-40 pl-1 ' />
     </div>
 
   </div>
   < div className='flex  flex-row'>
-  <div className='w-1/2 pr-2 mb-10'>Kilo:</div>
+  <div className='sm:w-1/2 w-1/5 pr-2  ml-20  sm:ml-0 mb-10'>Kilo:</div>
   <div>
   <input type="text" 
 className='bg border-gray-500  border-2 w-40' />
@@ -36,7 +36,7 @@ className='bg border-gray-500  border-2 w-40' />
 
   </div>
   <div className='flex flex-row mb-10'>
-    <div className='w-1/2 pr-2'>Yaş:</div>
+    <div className='sm:w-1/2 w-1/5  ml-20  sm:ml-0 pr-2'>Yaş:</div>
     <div>
     <input type="text" 
 className='bg border-gray-500  border-2 w-40' />
@@ -44,7 +44,7 @@ className='bg border-gray-500  border-2 w-40' />
 
   </div>
   <div className='flex flex-row'>
-    <div className='w-1/2 pr-2'>Cinsiyet</div>
+    <div className='sm:w-1/2 w-1/5 ml-20  sm:ml-0  pr-2'>Cinsiyet</div>
     <div >
       <select className='w-40 border-2 border-gray-500 ' value="Kadın">
       <option value="erkek">Erkek</option>
@@ -52,8 +52,8 @@ className='bg border-gray-500  border-2 w-40' />
       </select>
     </div>
   </div>
-  <div className='mt-10 sm:mx-auto'>
-  <button class="bg-red-400 hover:bg-red-300 text-black  py-2 px-4 border border-red-400 rounded">
+  <div className='mt-10 sm:ml-36 flex justify-center'>
+  <button class="bg-red-400 hover:bg-red-300 text-white  py-2 px-4 border border-red-400 rounded">
   Button
 </button>
   </div>
